b1cce8200b9dec96bb4d6a14b8bb14940132dc28
Martin Schuette <info@mschuette.name>
2008-08-13 00:54:39 +0000

    - move prototypes into headers - replace vsyslog_r() with call to vsyslogp_r() - add insert_fmt_m() to avoid duplicate code
    git-svn-id: file:///home/mschuett/syslog_repo/trunk@223 40eefb36-0fab-11dd-9f5d-b9dc37c5e4c2

--- src/libc_gen/syslog.c
+++ src/libc_gen/syslog.c
@@ -65,6 +65,8 @@ __weak_alias(openlog,_openlog)
 __weak_alias(setlogmask,_setlogmask)
 __weak_alias(syslog,_syslog)
 __weak_alias(vsyslog,_vsyslog)
+__weak_alias(syslogp,_syslogp)
+__weak_alias(vsyslogp,_vsyslogp)
 
 __weak_alias(closelog_r,_closelog_r)
 __weak_alias(openlog_r,_openlog_r)
@@ -73,25 +75,19 @@ __weak_alias(syslog_r,_syslog_r)
 __weak_alias(vsyslog_r,_vsyslog_r)
 __weak_alias(syslog_ss,_syslog_ss)
 __weak_alias(vsyslog_ss,_vsyslog_ss)
+__weak_alias(syslogp_r,_syslogp_r)
+__weak_alias(vsyslogp_r,_vsyslogp_r)
+__weak_alias(syslogp_ss,_syslogp_ss)
+__weak_alias(vsyslogp_ss,_vsyslogp_ss)
 #endif
-
-/* prototypes -- to be moved to .h */
-void syslogp(int pri, const char *msgid, const char *sdfmt, const char *msgfmt, ...);
-void vsyslogp(int pri, const char *msgid, const char *sdfmt, const char *msgfmt, va_list ap);
-void syslogp_r(int pri, struct syslog_data *data, const char *msgid,
-        const char *sdfmt, const char *msgfmt, ...);
-void syslogp_ss(int pri, struct syslog_data *data, const char *msgid,
-        const char *sdfmt, const char *msgfmt, ...);
-void vsyslogp_ss(int pri, struct syslog_data *data, const char *msgid,
-        const char *sdfmt, const char *msgfmt, va_list ap);
-void vsyslogp_r(int pri, struct syslog_data *data, const char *msgid,
-        const char *sdfmt, const char *msgfmt, va_list ap);
 
 static struct syslog_data sdata = SYSLOG_DATA_INIT;
 static void	openlog_unlocked_r(const char *, int, int,
     struct syslog_data *);
 static void	disconnectlog_r(struct syslog_data *);
 static void	connectlog_r(struct syslog_data *);
+static void     insert_fmt_m(const char *, char *, size_t *, const int, const int)
+static va_list	consume_va_args(const char *fmt0, va_list ap);
 
 #define LOG_SIGNAL_SAFE	(int)0x80000000
  
@@ -100,7 +96,6 @@ static mutex_t	syslog_mutex = MUTEX_INITIALIZER;
 #endif
 
 static char hostname[MAXHOSTNAMELEN];
-va_list consume_va_args(const char *fmt0, va_list ap);
 
 /*
  * syslog, vsyslog --
@@ -222,8 +217,16 @@ vsyslogp_ss(int pri, struct syslog_data *data, const char *msgid,
 void
 vsyslog_r(int pri, struct syslog_data *data, const char *fmt, va_list ap)
 {
+        vsyslogp_r(pri, data, NULL, NULL, fmt, ap);
+}
+
+void
+vsyslogp_r(int pri, struct syslog_data *data, const char *msgid,
+        const char *sdfmt, const char *msgfmt, va_list ap)
+{
         size_t cnt, prlen;
         char ch, *p, *t;
+        const char *fmt;
         time_t now;
         struct tm tmnow;
         int fd, saved_errno;
@@ -278,209 +281,6 @@ vsyslog_r(int pri, struct syslog_data *data, const char *fmt, va_list ap)
                 tbuf_left -= prlen;                             \
         } while (/*CONSTCOND*/0)
 
-        prlen = snprintf_ss(p, tbuf_left, "<%d>1 ", pri);
-        DEC();
-
-        if (!signal_safe) {
-                struct timeval tv;
-                /* strftime() implies tzset(), localtime_r() doesn't. */
-                tzset();
-                localtime_r(&now, &tmnow);
-
-                prlen = strftime(p, tbuf_left, "%FT%T", &tmnow);
-                DEC();
-                if (gettimeofday(&tv, NULL) != -1) {
-                        prlen = snprintf(p, tbuf_left, ".%06ld",
-                            tv.tv_usec);
-                        DEC();
-                }
-                prlen = strftime(p, tbuf_left-1, "%z", &tmnow);
-                /* strftime gives eg. "+0200", but we need "+02:00" */
-                if (prlen == 5) {
-                        p[prlen+1] = p[prlen];
-                        p[prlen]   = p[prlen-1];
-                        p[prlen-1] = p[prlen-2];
-                        p[prlen-2] = ':';
-                        prlen += 1;
-                }
-                DEC();
-                prlen = snprintf_ss(p, tbuf_left, " %s ", hostname);
-                DEC();
-        }
-
-        if (data->log_stat & LOG_PERROR)
-                stdp = p;
-        if (data->log_tag == NULL)
-                data->log_tag = getprogname();
-
-        prlen = snprintf_ss(p, tbuf_left, "%s ",
-            data->log_tag ? data->log_tag : "-");
-        DEC();
-
-        if (data->log_stat & LOG_PID)
-                prlen = snprintf_ss(p, tbuf_left, "%d ", getpid());
-        else
-                prlen = snprintf_ss(p, tbuf_left, "- ");
-        DEC();
-
-        /* 
-         * We wouldn't need this mess if printf handled %m, or if 
-         * strerror() had been invented before syslog().
-         */
-        for (t = fmt_cpy, fmt_left = FMT_LEN; (ch = *fmt) != '\0'; ++fmt) {
-                if (ch == '%' && fmt[1] == 'm') {
-                        char ebuf[128];
-                        ++fmt;
-                        if (signal_safe ||
-                            strerror_r(saved_errno, ebuf, sizeof(ebuf)))
-                                prlen = snprintf_ss(t, fmt_left, "Error %d", 
-                                    saved_errno);
-                        else
-                                prlen = snprintf_ss(t, fmt_left, "%s", ebuf);
-                        if (prlen >= fmt_left)
-                                prlen = fmt_left - 1;
-                        t += prlen;
-                        fmt_left -= prlen;
-                } else if (ch == '%' && fmt[1] == '%' && fmt_left > 2) {
-                        *t++ = '%';
-                        *t++ = '%';
-                        fmt++;
-                        fmt_left -= 2;
-                } else {
-                        if (fmt_left > 1) {
-                                *t++ = ch;
-                                fmt_left--;
-                        }
-                }
-        }
-        *t = '\0';
-
-        /* empty MSGID and SD fields */
-        prlen = snprintf_ss(p, tbuf_left, "- - ");
-        DEC();
-        
-        if (signal_safe)
-                prlen = vsnprintf_ss(p, tbuf_left, fmt_cpy, ap);
-        else
-                prlen = vsnprintf(p, tbuf_left, fmt_cpy, ap);
-        DEC();
-
-        cnt = p - tbuf;
-
-        /* Output to stderr if requested. */
-        if (data->log_stat & LOG_PERROR) {
-                struct iovec iov[2];
-
-                iov[0].iov_base = stdp;
-                iov[0].iov_len = cnt - (stdp - tbuf);
-                iov[1].iov_base = __UNCONST("\n");
-                iov[1].iov_len = 1;
-                (void)writev(STDERR_FILENO, iov, 2);
-        }
-
-        /* Get connected, output the message to the local logger. */
-        if (data == &sdata)
-                mutex_lock(&syslog_mutex);
-        if (!data->opened)
-                openlog_unlocked_r(data->log_tag, data->log_stat, 0, data);
-        connectlog_r(data);
-
-        /*
-         * If the send() failed, there are two likely scenarios:
-         *  1) syslogd was restarted
-         *  2) /dev/log is out of socket buffer space
-         * We attempt to reconnect to /dev/log to take care of
-         * case #1 and keep send()ing data to cover case #2
-         * to give syslogd a chance to empty its socket buffer.
-         */
-        if (send(data->log_file, tbuf, cnt, 0) == -1) {
-                if (errno != ENOBUFS) {
-                        disconnectlog_r(data);
-                        connectlog_r(data);
-                }
-                do {
-                        usleep(1);
-                        if (send(data->log_file, tbuf, cnt, 0) != -1)
-                                break;
-                } while (errno == ENOBUFS);
-        }
-        if (data == &sdata)
-                mutex_unlock(&syslog_mutex);
-
-        /*
-         * Output the message to the console; try not to block
-         * as a blocking console should not stop other processes.
-         * Make sure the error reported is the one from the syslogd failure.
-         */
-        if ((data->log_stat & LOG_CONS) &&
-            (fd = open(_PATH_CONSOLE, O_WRONLY|O_NONBLOCK, 0)) >= 0) {
-                struct iovec iov[2];
-                
-                p = strchr(tbuf, '>') + 1;
-                iov[0].iov_base = p;
-                iov[0].iov_len = cnt - (p - tbuf);
-                iov[1].iov_base = __UNCONST("\r\n");
-                iov[1].iov_len = 2;
-                (void)writev(fd, iov, 2);
-                (void)close(fd);
-        }
-        if (data != &sdata)
-                closelog_r(data);
-}
-
-void
-vsyslogp_r(int pri, struct syslog_data *data, const char *msgid,
-        const char *sdfmt, const char *msgfmt, va_list ap)
-{
-        size_t cnt, prlen;
-        char ch, *p, *t;
-        const char *fmt;
-        time_t now;
-        struct tm tmnow;
-        int fd, saved_errno;
-#define TBUF_LEN        2048
-#define FMT_LEN         1024
-        char *stdp = NULL;      /* pacify gcc */
-        char tbuf[TBUF_LEN], fmt_cpy[FMT_LEN];
-        size_t tbuf_left, fmt_left;
-        int signal_safe = pri & LOG_SIGNAL_SAFE;
-
-        pri &= ~LOG_SIGNAL_SAFE;
-
-#define INTERNALLOG     LOG_ERR|LOG_CONS|LOG_PERROR|LOG_PID
-        /* Check for invalid bits. */
-        if (pri & ~(LOG_PRIMASK|LOG_FACMASK)) {
-                syslog_r(INTERNALLOG | signal_safe, data,
-                    "syslogp_r: unknown facility/priority: %x", pri);
-                pri &= LOG_PRIMASK|LOG_FACMASK;
-        }
-
-        /* Check priority against setlogmask values. */
-        if (!(LOG_MASK(LOG_PRI(pri)) & data->log_mask))
-                return;
-
-        saved_errno = errno;
-
-        /* Set default facility if none specified. */
-        if ((pri & LOG_FACMASK) == 0)
-                pri |= data->log_fac;
-
-        /* Build the message. */
-        
-        /*
-         * Although it's tempting, we can't ignore the possibility of
-         * overflowing the buffer when assembling the "fixed" portion
-         * of the message.  Strftime's "%h" directive expands to the
-         * locale's abbreviated month name, but if the user has the
-         * ability to construct to his own locale files, it may be
-         * arbitrarily long.
-         */
-         if (!signal_safe)
-                (void)time(&now);
-
-        p = tbuf;  
-        tbuf_left = TBUF_LEN;
-        
         prlen = snprintf_ss(p, tbuf_left, "<%d>1 ", pri);
         DEC();
 
@@ -547,38 +347,8 @@ vsyslogp_r(int pri, struct syslog_data *data, const char *msgid,
         if (fmt == NULL)
                 prlen = snprintf_ss(p, tbuf_left, "- ");
         else {
-                /* 
-                 * We wouldn't need this mess if printf handled %m, or if 
-                 * strerror() had been invented before syslog().
-                 */
-                for (t = fmt_cpy, fmt_left = FMT_LEN; (ch = *fmt) != '\0'; ++fmt) {
-                        if (ch == '%' && fmt[1] == 'm') {
-                                char ebuf[128];
-                                ++fmt;
-                                if (signal_safe ||
-                                    strerror_r(saved_errno, ebuf, sizeof(ebuf)))
-                                        prlen = snprintf_ss(t, fmt_left, "Error %d", 
-                                            saved_errno);
-                                else
-                                        prlen = snprintf_ss(t, fmt_left, "%s", ebuf);
-                                if (prlen >= fmt_left)
-                                        prlen = fmt_left - 1;
-                                t += prlen;
-                                fmt_left -= prlen;
-                        } else if (ch == '%' && fmt[1] == '%' && fmt_left > 2) {
-                                *t++ = '%';
-                                *t++ = '%';
-                                fmt++;
-                                fmt_left -= 2;
-                        } else {
-                                if (fmt_left > 1) {
-                                        *t++ = ch;
-                                        fmt_left--;
-                                }
-                        }
-                }
-                *t = '\0';
-        
+                insert_fmt_m(fmt, fmt_cpy, &prlen, saved_errno, signal_safe);
+
                 if (signal_safe)
                         prlen = vsnprintf_ss(p, tbuf_left, fmt_cpy, ap);
                 else
@@ -589,34 +359,8 @@ vsyslogp_r(int pri, struct syslog_data *data, const char *msgid,
         
         fmt = msgfmt;
         if (fmt != NULL) {
-                for (t = fmt_cpy, fmt_left = FMT_LEN; (ch = *fmt) != '\0'; ++fmt) {
-                        if (ch == '%' && fmt[1] == 'm') {
-                                char ebuf[128];
-                                ++fmt;
-                                if (signal_safe ||
-                                    strerror_r(saved_errno, ebuf, sizeof(ebuf)))
-                                        prlen = snprintf_ss(t, fmt_left, "Error %d", 
-                                            saved_errno);
-                                else
-                                        prlen = snprintf_ss(t, fmt_left, "%s", ebuf);
-                                if (prlen >= fmt_left)
-                                        prlen = fmt_left - 1;
-                                t += prlen;
-                                fmt_left -= prlen;
-                        } else if (ch == '%' && fmt[1] == '%' && fmt_left > 2) {
-                                *t++ = '%';
-                                *t++ = '%';
-                                fmt++;
-                                fmt_left -= 2;
-                        } else {
-                                if (fmt_left > 1) {
-                                        *t++ = ch;
-                                        fmt_left--;
-                                }
-                        }
-                }
-                *t = '\0';
-        
+                insert_fmt_m(fmt, fmt_cpy, &prlen, saved_errno, signal_safe);
+
                 if (signal_safe)
                         prlen = vsnprintf_ss(p, tbuf_left, fmt_cpy, ap);
                 else
@@ -799,6 +543,48 @@ setlogmask_r(int pmask, struct syslog_data *data)
 	return omask;
 }
 
+/* 
+ * We wouldn't need this mess if printf handled %m, or if 
+ * strerror() had been invented before syslog().
+ */
+static void
+insert_fmt_m(const char *fmt, char *fmt_cpy, size_t *prlen_ptr,
+        const int saved_errno, const int signal_safe)
+{
+        char ch;
+        char *t;
+        size_t fmt_left;
+        size_t prlen = *prlen_ptr;
+
+        for (t = fmt_cpy, fmt_left = FMT_LEN; (ch = *fmt) != '\0'; ++fmt) {
+                if (ch == '%' && fmt[1] == 'm') {
+                        char ebuf[128];
+                        ++fmt;
+                        if (signal_safe ||
+                            strerror_r(saved_errno, ebuf, sizeof(ebuf)))
+                                prlen = snprintf_ss(t, fmt_left, "Error %d", 
+                                    saved_errno);
+                        else
+                                prlen = snprintf_ss(t, fmt_left, "%s", ebuf);
+                        if (prlen >= fmt_left)
+                                prlen = fmt_left - 1;
+                        t += prlen;
+                        fmt_left -= prlen;
+                } else if (ch == '%' && fmt[1] == '%' && fmt_left > 2) {
+                        *t++ = '%';
+                        *t++ = '%';
+                        fmt++;
+                        fmt_left -= 2;
+                } else {
+                        if (fmt_left > 1) {
+                                *t++ = ch;
+                                fmt_left--;
+                        }
+                }
+        }
+        *t = '\0';
+}
+
 /* hack to prevent lint from complaining */
 static __inline void __use(int x, ...) { /* LINTED */ (void)x; }
 #define __USED(...) __use(0, __VA_ARGS__);
@@ -810,7 +596,7 @@ static __inline void __use(int x, ...) { /* LINTED */ (void)x; }
  * consumes all used arguments from
  * the va_list
  */
-va_list
+static va_list
 consume_va_args(const char *fmt0, va_list ap)
 {
         const char *fmt = fmt0;
